1a1504ff0599c81f0795300e98b1a45cf046d012
krupali makadiya <krupalimakadiya123@gmail.com>
2019-08-04 10:24:45 +0530

    Updates

---
+++ precache-manifest.5bdd3612e2399fa7fcebe4c644e4ed97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff52a116e183cff47acf7b88059863d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e834c0d33c03da513cb",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "2e834c0d33c03da513cb",
+    "url": "/static/js/main.d44ec6fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
